10402d486a89a238fc24a82ce32d9b6d77fb1f52
gopala01 <bhamidipatig@gmail.com>
2024-09-28 17:32:57 +0100

    second commit

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import styles from "./HeroStyles.module.css";
+import heroImg from "../../assets/hero-img.png";
+
+function Hero() {
+  return (
+    <section id="hero">
+      <div className={styles.colorModeContainer}>
+        <img
+          className={styles.hero}
+          src={heroImg}
+          alt="Profile Picture of Gopala Bhamidipati"
+        ></img>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
