8982e7cb1826b9c3fcf2fb58aff5b59aa4665f03
gopala01 <bhamidipatig@gmail.com>
2025-11-05 11:06:33 +0000

    Update About section with enhanced personal and professional details

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -8,20 +8,39 @@ function About() {
       <div className={styles.content}>
         <p>
           Hi, I'm <strong>Gopala Bhamidipati</strong>! I'm a computer science
-          student passionate about software development and AI based in London,
-          UK. I graduated with <strong>First Class Honours</strong> in Computer
-          Science from Queen Mary University of London and have completed an{" "}
+          graduate passionate about <strong>data engineering</strong> and
+          <strong> software development</strong>, based in London, UK. I have
+          completed an{" "}
+          <strong>MSc in Artificial Intelligence and Data Engineering</strong>
+          at UCL and graduated with <strong>First Class Honours</strong> in
+          Computer Science from Queen Mary University of London.
+          <br />
+          <br />
+          I’ve worked with the{" "}
           <strong>
-            MSc in Artificial Intelligence and Data Engineering at UCL.
+            International Federation of Red Cross and Red Crescent Societies
+            (IFRC)
           </strong>
+          as an <strong>AI Software Engineer</strong>, where I deployed Azure
+          OpenAI–powered summarisation modules that transformed emergency data
+          into concise insights — cutting report analysis time from hours to
+          minutes.
+          <br />
+          <br />
           This portfolio showcases my{" "}
-          <strong>work, skills, and projects</strong>. Built with{" "}
-          <strong>React, JavaScript, and Vanilla CSS</strong>, it reflects my
-          commitment to writing{" "}
-          <strong>clean, efficient and maintainable code</strong>, while
+          <strong>
+            projects, technical skills, and creative problem-solving
+          </strong>
+          — from distributed data automation pipelines to interactive web
+          applications. Built with{" "}
+          <strong>React, JavaScript, and Vanilla CSS</strong>, this portfolio
+          reflects my commitment to writing{" "}
+          <strong>clean, efficient, and maintainable code</strong>, while
           ensuring my applications are{" "}
-          <strong>scalable, responsive and user-friendly</strong>. Feel free to
-          explore!
+          <strong>scalable, responsive, and user-friendly</strong>.
+          <br />
+          <br />
+          Feel free to explore! 🚀
         </p>
         <div className={styles.colorModeContainer}>
           <img
